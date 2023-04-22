00783f0e5bf7fd364a263d2d7c7a6c5135e4d108
TahianSylvain <ranjalahyandrytahianasylvain@gmail.com>
2023-04-23 00:40:26 +0300

    Cleaned

--- principal/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Annotation
-
-admin.site.register(Annotation)
--- principal/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Annotation
-
-
-class AnnotationForm(forms.ModelForm):
-    class Meta:
-        model = Annotation
-        fields = ['name', 'description', 'deadline', 'reminder']
